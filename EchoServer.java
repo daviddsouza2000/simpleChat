5604eb4e4e0dc7ae3a6b125675e8e790e5d075ab
David D'Souza <1david.dsouza.2000@gmail.com>
2020-10-29 21:48:59 -0400

    part 1

--- EchoServer.java
+++ EchoServer.java
@@ -45,8 +45,7 @@ public class EchoServer extends AbstractServer
    * @param msg The message received from the client.
    * @param client The connection from which the message originated.
    */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
+  public void handleMessageFromClient(Object msg, ConnectionToClient client)
   {
     System.out.println("Message received: " + msg + " from " + client);
     this.sendToAllClients(msg);
@@ -70,6 +69,22 @@ public class EchoServer extends AbstractServer
   {
     System.out.println
       ("Server has stopped listening for connections.");
+  }
+  
+  
+  /**
+   * Hook method called each time a new client connection is
+   * accepted.
+   * @param client the connection connected to the client.
+   */
+  @Override
+  protected void clientConnected(ConnectionToClient client) {
+	  System.out.println("Client has connected");
+  }
+  
+  @Override
+  synchronized protected void clientDisconnected(ConnectionToClient client) {
+	  System.out.println("Client has disconnected");
   }
   
   //Class methods ***************************************************
--- client/ChatClient.java
+++ client/ChatClient.java
@@ -90,5 +90,25 @@ public class ChatClient extends AbstractClient
     catch(IOException e) {}
     System.exit(0);
   }
+  /**
+   * Hook method called after the connection has been closed.
+   */
+  @Override
+  protected void connectionClosed() {
+	  clientUI.display("The connection has been closed");
+  }
+	
+  /**
+   * Hook method called each time an exception is thrown by the client's
+   * thread that is waiting for messages from the server. 
+   * @param exception
+   *            the exception raised.
+   */
+  @Override
+  protected void connectionException(Exception exception) {
+	  clientUI.display("The server has shut down");
+  }
 }
+
+
 //End of ChatClient class
